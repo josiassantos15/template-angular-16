a009a130ce77be54f63b4825d51b7ca61811b86d
josiassantos15 <josiassantos1577@gmail.com>
2025-05-11 23:49:32 -0300

    feat: Atualizando footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
+  currentYear: number;
+
+  constructor() {
+    this.currentYear = new Date().getFullYear();
+  }
 
 }
